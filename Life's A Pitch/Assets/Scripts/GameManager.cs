3dbeb28396d1ef018752230c88c11252bce0a1fe
timrcoding <59135555+timrcoding@users.noreply.github.com>
2020-10-19 08:47:49 +0100

    news feed setup

---
+++ Life's A Pitch/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    //NEWS
+    [SerializeField]
+    private GameObject Teleprinter;
+    [SerializeField]
+    public float newsSpawnSpeed;
+
+    void Start()
+    {
+        instance = this;
+        StartCoroutine(createNews());
+    }
+
+    IEnumerator createNews()
+    {
+        yield return new WaitForSeconds(newsSpawnSpeed);
+        GameObject NewsPrinter = GameObject.FindGameObjectWithTag("NewsPrinter");
+        GameObject news = Instantiate(Teleprinter, NewsPrinter.transform.position, Teleprinter.transform.rotation);
+        news.transform.SetParent(NewsPrinter.transform, false);
+        //news.GetComponent<NewsfeedBehaviour>().newsText.text = generateGenericFeed();
+
+        StartCoroutine(createNews());
+    }
+
+}
